df29408aa4c75d6358edff2f33631b10adfca2ba
tianye19941001 <838199892@qq.com>
2017-02-21 16:11:14 +0800

    ty

--- 20170208-sell/src/App.vue
+++ 20170208-sell/src/App.vue
@@ -2,13 +2,17 @@
   <div id="app">
     <v-header></v-header>
     <div class="tab">
-      <div class="tab-item">商品</div>
-      <div class="tab-item">评论</div>
-      <div class="tab-item">商家</div>
+      <div class="tab-item">
+        <a v-link="{path:'goods'}">商品</a>
+      </div>
+      <div class="tab-item">
+        <a v-link="{path:'ratings'}">评论</a>
+      </div>
+      <div class="tab-item">
+        <a v-link="{path:'seller'}">商家</a>
+      </div>
     </div>
-    <div class="content">
-      I am content~
-    </div>
+    <router-view></router-view>
   </div>
 </template>
 
--- 20170208-sell/src/main.js
+++ 20170208-sell/src/main.js
@@ -1,11 +1,20 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App';
+import goods from './components/goods/goods';
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+Vue.use(VueRouter);
+
+const routes = [
+	{ path: '/goods', components: goods }
+];
+
+const router = new VueRouter({
+	routes // （缩写）相当于 routes: routes
 });
+
+let app = new Vue({
+	router
+}).$mount('#app');
