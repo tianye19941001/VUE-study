2cdca9f949f3dbcbef076c7831e65b7b472d6323
tianye19941001 <838199892@qq.com>
2017-02-21 10:58:00 +0800

    ty

--- 20170208-sell/src/App.vue
+++ 20170208-sell/src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <v-header></v-header>
     <div class="tab">
-      I am tab~
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
     </div>
     <div class="content">
       I am content~
@@ -19,13 +21,17 @@
   };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" rel="stylesheet/stylus">
+#app
+  .tab
+    display: flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    text-align: center
+    .tab-item
+      flex:1
+
+
+
 </style>
