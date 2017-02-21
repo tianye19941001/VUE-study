86ad16a26fa701beb3e7ed4747938ee22bf28cbc
tianye19941001 <838199892@qq.com>
2017-02-21 10:11:17 +0800

    ty

--- 20170208-sell/src/App.vue
+++ 20170208-sell/src/App.vue
@@ -1,14 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <v-header></v-header>
+    <div class="tab">
+      I am tab~
+    </div>
+    <div class="content">
+      I am content~
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import header from './components/header/header.vue';
+  export default{
+    components: {
+      'v-header': header
+    }
+  };
 </script>
 
 <style>
